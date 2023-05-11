7b38de814692edb426265b7740d082cd5c658d91
Huszan <mateuszjacenty1@gmail.com>
2023-05-11 09:04:36 +0200

    Improve http calls

--- src/app/services/http/manga-http.service.ts
+++ src/app/services/http/manga-http.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { IMangaForm } from '../../types/manga-form.type';
 import { MangaType } from '../../types/manga.type';
 
 const MANGA_DOMAIN = {
@@ -32,45 +31,47 @@ export class MangaHttpService {
     }
   }
 
-  private _routeUrl(route: MANGA_ROUTE) {
-    return this._domain + route;
+  private _routeUrl(route: MANGA_ROUTE): URL {
+    return new URL(this._domain + route);
   }
 
   getMangaList(id?: number): Observable<any> {
     let route = this._routeUrl(MANGA_ROUTE.GET_MANGAS);
-    if (id != undefined) route += `?id=${id}`;
-    return this.http.get(route);
+    if (id) {
+      route.searchParams.set('id', String(id));
+    }
+    return this.http.get(route.toString());
   }
 
   getMangaPages(manga: MangaType, chapter: number): Observable<any> {
-    return this.http.post(this._routeUrl(MANGA_ROUTE.GET_PAGES), {
+    return this.http.post(this._routeUrl(MANGA_ROUTE.GET_PAGES).toString(), {
       manga: manga,
       chapter: chapter,
     });
   }
 
   testMangaForm(mangaForm: any, testId?: number | undefined): Observable<any> {
-    return this.http.post(this._routeUrl(MANGA_ROUTE.TEST_FORM), {
+    return this.http.post(this._routeUrl(MANGA_ROUTE.TEST_FORM).toString(), {
       manga: mangaForm,
       testId: testId,
     });
   }
 
   testMangaChapter(mangaForm: any, chapter: number): Observable<any> {
-    return this.http.post(this._routeUrl(MANGA_ROUTE.TEST_CHAPTER), {
+    return this.http.post(this._routeUrl(MANGA_ROUTE.TEST_CHAPTER).toString(), {
       manga: mangaForm,
       chapter: chapter,
     });
   }
 
   postManga(mangaForm: any): Observable<any> {
-    return this.http.post(this._routeUrl(MANGA_ROUTE.POST), {
+    return this.http.post(this._routeUrl(MANGA_ROUTE.POST).toString(), {
       manga: mangaForm,
     });
   }
 
   removeManga(manga: any): Observable<any> {
-    return this.http.post(this._routeUrl(MANGA_ROUTE.REMOVE), {
+    return this.http.post(this._routeUrl(MANGA_ROUTE.REMOVE).toString(), {
       manga: manga,
     });
   }
